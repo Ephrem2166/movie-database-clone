3d633331bf9d342ae6a348cad98da8a18db032d8
Ephrem2166 <ephrem2181@gmail.com>
2023-09-06 16:00:32 +0300

    spinner and popular tv shows

--- js/script.js
+++ js/script.js
@@ -37,18 +37,62 @@ async function displayPopularMovies() {
   });
 }
 
+async function displayPopularShows() {
+  const { results } = await fetchAPIData("tv/popular");
+  results.forEach((show) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `
+    
+          <a href="tv-details.html?id=${show.id}">
+          ${
+            show.poster_path
+              ? `          <img
+              src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+              class="card-img-top"
+              alt="${show.name}"
+            />`
+              : `          <img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="Movie Title"
+            />`
+          }  
+
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${show.name}</h5>
+            <p class="card-text">
+              <small class="text-muted">Air Date: ${show.first_air_date}</small>
+            </p>
+          </div>
+  
+    `;
+    document.querySelector("#popular-shows").appendChild(div);
+  });
+}
+
 // Fetch data from TMDB
 async function fetchAPIData(endpoint) {
   const API_KEY = "d33039110fc10876bd2ec50db7309652";
   const API_URL = "https://api.themoviedb.org/3/";
 
+  showSpinner();
   const response = await fetch(
     `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
   );
 
   const data = await response.json();
-
+  hideSpinner();
   return data;
+}
+
+function showSpinner() {
+  document.querySelector(".spinner").classList.add("show");
+}
+
+function hideSpinner() {
+  document.querySelector(".spinner").classList.remove("show");
 }
 
 // Highlight active links
@@ -69,6 +113,7 @@ function init() {
       console.log("Home");
       break;
     case "/shows.html":
+      displayPopularShows();
       console.log("Shows");
       break;
     case "/movie-details.html":
